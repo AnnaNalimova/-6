69caa9fddf41c92c1bc1a809f2a146d535e57fca
Anna Nalimova <avnalimova@mail.ru>
2023-02-13 17:40:16 +0300

    Initia commit

---
+++ Test/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+if(number%2==0 ){
+    Console.WriteLine("Четное число");
+}
+else
+{
+    Console.WriteLine("Нечетное число");
+}
